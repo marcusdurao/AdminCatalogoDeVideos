891c9fc79f2915e9f641ac5769666d7345497369
Marcus Durao <mvdurao@gmail.com>
2023-01-03 20:05:24 -0300

    Modulo 02 - Implementando o use case de Categoria

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/category/delete/DefaultDeleteCategoryUseCase.java
@@ -0,0 +1,16 @@
+package com.fullcycle.admin.catalogo.application.category.delete;
+
+import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
+import com.fullcycle.admin.catalogo.domain.category.CategoryID;
+
+public class DefaultDeleteCategoryUseCase extends DeleteCategoryUseCase{
+
+    private final CategoryGateway categoryGateway;
+    public DefaultDeleteCategoryUseCase(final CategoryGateway categoryGateway) {
+        this.categoryGateway = categoryGateway;
+    }
+    @Override
+    public void execute(final String anId) {
+        this.categoryGateway.deleteById(CategoryID.from(anId));
+    }
+}
---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/category/retrieve/get/GetCategoryByIdUseCase.java
@@ -0,0 +1,5 @@
+package com.fullcycle.admin.catalogo.application.category.retrieve.get;
+
+import com.fullcycle.admin.catalogo.application.UseCase;
+
+public abstract class GetCategoryByIdUseCase extends UseCase<String, CategoryOutput> {}
